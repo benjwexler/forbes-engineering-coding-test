c2e1c49e23c2a4fa49404df0c28d34d28113ad9e
Ben Wexler <mixedywex@gmail.com>
2021-07-25 06:03:31 -0400

    add paginationg and some styling

--- src/index.js
+++ src/index.js
@@ -4,34 +4,38 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import "./styles.css";
 
- function component() {
 
-   const fetchImages = async () => {
-     const queryParamsObj = {
-       key: '22638056-d088870190bfd9698c73834e3',
-       q: 'code',
-       orientation: "vertical",
-       'per_page': 200,
-     }
+  let images = [];
+  let currentPage = 1;
+  const imagesPerPage = 10;
 
-     const addQueryParams = (paramsObj) => {
-        const keys = Object.keys(paramsObj);
-        return keys.map(key => `${key}=${paramsObj[key]}`).join('&')
-     }
+  const fetchImages = async ({ page = 1, perPage }) => {
+    const queryParamsObj = {
+      key: '22638056-d088870190bfd9698c73834e3',
+      q: 'tennis',
+      orientation: "vertical",
+      'per_page': perPage,
+      page,
+    }
 
-     const queryParams = addQueryParams(queryParamsObj);
-     const baseUrl = 'https://pixabay.com/api';
-     const fullUrl = `${baseUrl}?${queryParams}`;
-     
+    const addQueryParams = (paramsObj) => {
+      const keys = Object.keys(paramsObj);
+      return keys.map(key => `${key}=${paramsObj[key]}`).join('&')
+    }
+
+    const queryParams = addQueryParams(queryParamsObj);
+    const baseUrl = 'https://pixabay.com/api';
+    const fullUrl = `${baseUrl}?${queryParams}`;
+
     const res = await fetch(fullUrl);
     const data = await res.json()
     // console.log('res', await res.json())
     return data.hits;
-   }
+  }
 
   //  fetchImages();
 
-   const addThumbnail = (imgSrc) => {
+  const addThumbnail = (imgSrc) => {
     const figure = document.createElement('div');
     figure.className = "photo";
     const img = document.createElement('img');
@@ -41,30 +45,78 @@ import "./styles.css";
 
     const imagesContainer = document.getElementById('images--container')
 
-    document.body.append(imagesContainer)
-    
+    // document.body.append(imagesContainer)
+
     imagesContainer.append(figure)
 
-   }
+  }
 
   //  addThumbnail()
 
-   const fetchImagesAndRenderToDom = async () => {
-    const images = await fetchImages();
+  const setButtonEnabledStatusAfterPageChange = (btnElement, bool) => {
+    console.log('btn', btnElement)
+    if (!btnElement) return;
 
-    images.forEach(image => {
-      console.log('image', image)
+    btnElement.disabled = !bool;
+
+  }
+
+  const fetchImagesAndRenderToDom = async (pageNum) => {
+    const images = await fetchImages({ page: pageNum, perPage: imagesPerPage });
+
+    console.log('image.length', images.length)
+    const nextBtn = document.getElementById('btn--next');
+    // If the number of images is less than imagesPerPage we are making the assumption we are on the last page
+    setButtonEnabledStatusAfterPageChange(nextBtn, images.length >= imagesPerPage)
+
+    images.slice(0, 10).forEach(image => {
+      // console.log('image', image)
       addThumbnail(image.previewURL)
     })
 
 
-   }
+  }
 
-   fetchImagesAndRenderToDom()
-
-
+  const removeElements = (elms) => elms.forEach(el => el.remove());
 
 
- }
 
- document.body.appendChild(component());
+
+
+  fetchImagesAndRenderToDom(currentPage)
+  const nextBtn = document.getElementById('btn--next');
+
+  nextBtn.addEventListener('click', () => {
+
+    currentPage += 1
+
+    const previousBtn = document.getElementById('btn--previous');
+    setButtonEnabledStatusAfterPageChange(previousBtn, currentPage > 1)
+    removeElements(document.querySelectorAll(".photo"));
+
+    fetchImagesAndRenderToDom(currentPage)
+
+  })
+
+
+  const previousBtn = document.getElementById('btn--previous');
+  previousBtn.addEventListener('click', () => {
+
+
+    if (currentPage > 1) {
+      currentPage -= 1
+    } else {
+      currentPage = 1;
+    }
+
+    console.log('currentPage', currentPage)
+
+    setButtonEnabledStatusAfterPageChange(previousBtn, currentPage > 1)
+
+    removeElements(document.querySelectorAll(".photo"));
+
+    fetchImagesAndRenderToDom(currentPage)
+
+  })
+
+// document.body.appendChild(component());
